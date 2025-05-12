b1065622984b5ed9634413e7961b859b713bf1d3
amura870 <muramatsu-atsuya870@g.ecc.u-tokyo.ac.jp>
2025-05-12 22:35:25 +0900

    feat: init backend with spring boot initializr

---
+++ backend/src/test/java/com/team8/taak/TaakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team8.taak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
