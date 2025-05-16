ae75f9ca13ae255b12cb00c964e1ea396a86b6ee
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2025-05-16 22:40:27 +0900

    devtoolsの位置を変更

--- frontend_react/src/app/routes/__root.tsx
+++ frontend_react/src/app/routes/__root.tsx
@@ -1,11 +1,18 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
+      <nav className="bg-background fixed right-0 bottom-0 left-0 flex h-16 gap-4 border-t p-4">
+        <Link to="/">Home</Link>
+        <Link to="/diary">Diary</Link>
+        <Link to="/todo">Todo</Link>
+      </nav>
+      <TanStackRouterDevtools
+        toggleButtonProps={{ class: 'mb-[calc(64px)]' }}
+      />
     </>
   ),
 });
