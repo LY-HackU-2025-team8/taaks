3fe21900dccc465a86c80d9d21ac6f04836472a8
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2025-05-16 18:21:20 +0900

    各タブの作成

---
+++ frontend/app/(tabs)/diary.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function DiaryScreen() {
+  return (
+    <ThemedView>
+      <ThemedText>Diary</ThemedText>
+    </ThemedView>
+  );
+}
