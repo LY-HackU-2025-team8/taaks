c01bc49e29228f59514d4eaa0925871683fccef6
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2025-05-17 17:07:44 +0900

    カスタムカラーへの対応とフルスクリーンAppへの変更

--- frontend_react/src/app/routes/todo.tsx
+++ frontend_react/src/app/routes/todo.tsx
@@ -1,3 +1,5 @@
+import { PageHeader } from '@/shared/ui/page/page-header';
+import { PageMain } from '@/shared/ui/page/page-main';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/todo')({
@@ -6,8 +8,16 @@ export const Route = createFileRoute('/todo')({
 
 function RouteComponent() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">This is Todo</h1>
+    <div className="flex h-full flex-col">
+      <PageHeader className="bg-custom text-custom-foreground rounded-b-[32px] pb-8">
+        <div className="flex items-center px-[13px] py-3.5">
+          <h1 className="text-[28px] font-bold">Todo</h1>
+        </div>
+        <div className="p-3.5">
+          <h3 className="text-2xl font-bold">4月</h3>
+        </div>
+      </PageHeader>
+      <PageMain className="flex-1">本体</PageMain>
     </div>
   );
 }
