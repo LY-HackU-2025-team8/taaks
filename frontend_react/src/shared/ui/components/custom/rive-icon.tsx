ec976bb034e41e76691f72704dcae86e69038280
naokeyn <naokey-1228@outlook.jp>
2025-05-22 02:04:17 +0900

    RiveIconのフォーマット

--- frontend_react/src/shared/ui/components/custom/rive-icon.tsx
+++ frontend_react/src/shared/ui/components/custom/rive-icon.tsx
@@ -1,36 +1,40 @@
-import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
 import { useState } from 'react';
+import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
 
 export type RiveIconProps = {
   /** クラス名 */
   className?: string;
   /** アイコンタイプ */
-  iconType: "Home" | "ToDo" | "Diary" | "Account" | "PLUS";
+  iconType: 'Home' | 'ToDo' | 'Diary' | 'Account' | 'PLUS';
   /** アイコンの状態 */
   isActive?: boolean;
-}
+};
 
-export const RiveIcon = ({ className = '', iconType, isActive = false }: RiveIconProps) => {
+export const RiveIcon = ({
+  className = '',
+  iconType,
+  isActive = false,
+}: RiveIconProps) => {
   const [isClick, setIsClick] = useState<boolean>(isActive);
 
   const ICON_TYPES = {
-    Home: {stateMachineName: 'HomeState', inputName: 'home_inputs'},
-    ToDo: {stateMachineName: 'ToDoState', inputName: 'ToDo_inputs'},
-    Diary: {stateMachineName: 'DiaryState', inputName: 'Diary_inputs'},
-    Account: {stateMachineName: 'AccountState', inputName: 'Account_inputs'},
-    PLUS: {stateMachineName: 'PlusState', inputName: 'PLUS_inputs'},
-  }
+    Home: { stateMachineName: 'HomeState', inputName: 'home_inputs' },
+    ToDo: { stateMachineName: 'ToDoState', inputName: 'ToDo_inputs' },
+    Diary: { stateMachineName: 'DiaryState', inputName: 'Diary_inputs' },
+    Account: { stateMachineName: 'AccountState', inputName: 'Account_inputs' },
+    PLUS: { stateMachineName: 'PlusState', inputName: 'PLUS_inputs' },
+  };
 
   const { rive, RiveComponent } = useRive({
     src: '/assets/animations/icon_animation.riv',
     stateMachines: ICON_TYPES[iconType].stateMachineName,
     autoplay: true,
     artboard: iconType,
-  })
+  });
 
   const input = useStateMachineInput(
-    rive, 
-    ICON_TYPES[iconType].stateMachineName, 
+    rive,
+    ICON_TYPES[iconType].stateMachineName,
     ICON_TYPES[iconType].inputName
   );
 
@@ -39,12 +43,10 @@ export const RiveIcon = ({ className = '', iconType, isActive = false }: RiveIco
       input.value = isClick ? 0 : 1;
       setIsClick(!isClick);
     }
-  }
+  };
 
   return (
-    <button
-      onClick={onClick}
-    >
+    <button onClick={onClick}>
       <RiveComponent
         className={className}
         role="img"
