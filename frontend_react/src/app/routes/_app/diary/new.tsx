64f4fd4b2f134a7590472517a9d8d63cddda1698
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2025-05-24 14:56:38 +0900

    ページスタイルの変更

--- frontend_react/src/app/routes/_app/diary/new.tsx
+++ frontend_react/src/app/routes/_app/diary/new.tsx
@@ -1,9 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { CloseIcon } from '@/shared/ui/components/icons/close-icon';
+import { Button } from '@/shared/ui/components/shadcn/button';
+import { PageHeader } from '@/shared/ui/page/page-header';
+import { PageMain } from '@/shared/ui/page/page-main';
+import { PageTitleContainer } from '@/shared/ui/page/page-title-container';
+import { createFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_app/diary/new')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div className="h-full"></div>;
+  return (
+    <>
+      <PageHeader>
+        <PageTitleContainer>
+          <Button variant="ghost" size="icon" asChild>
+            <Link to="/diary">
+              <CloseIcon />
+            </Link>
+          </Button>
+        </PageTitleContainer>
+      </PageHeader>
+      <PageMain></PageMain>
+    </>
+  );
 }
--- frontend_react/src/shared/ui/page/page-main.tsx
+++ frontend_react/src/shared/ui/page/page-main.tsx
@@ -6,7 +6,7 @@ export const PageMain = ({ className, ...props }: PageMainProps) => {
   return (
     <main
       className={cn(
-        'flex flex-1 flex-col pt-[env(safe-area-inset-top)] pr-[env(safe-area-inset-right)] pl-[env(safe-area-inset-left)]',
+        'mr-[env(safe-area-inset-right)] ml-[env(safe-area-inset-left)] p-3.5',
         className
       )}
       {...props}
