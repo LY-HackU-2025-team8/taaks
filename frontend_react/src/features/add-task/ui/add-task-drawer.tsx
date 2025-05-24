e6f6b6e3282e2649a2cf189b2c059ce09695d7fb
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2025-05-24 19:43:26 +0900

    タスク追加時にタスク一覧がリセットされるように

--- frontend_react/src/features/add-task/ui/add-task-drawer.tsx
+++ frontend_react/src/features/add-task/ui/add-task-drawer.tsx
@@ -25,6 +25,7 @@ import { Switch } from '@/shared/ui/components/shadcn/switch';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useQueryClient } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import { LucideClock, LucideNotepadText } from 'lucide-react';
 import { z } from 'zod';
@@ -51,7 +52,12 @@ export const AddTaskDrawer = ({
   open: propsOpen = false,
   triggerComponent,
 }: AddTaskDrawerProps) => {
-  const { mutate, isPending, error } = $api.useMutation('post', '/tasks');
+  const queryClient = useQueryClient();
+  const { mutate, isPending, error } = $api.useMutation('post', '/tasks', {
+    onSettled: () => {
+      queryClient.invalidateQueries($api.queryOptions('get', '/tasks'));
+    },
+  });
   const [open, setOpen] = useState(propsOpen);
 
   useEffect(() => {
