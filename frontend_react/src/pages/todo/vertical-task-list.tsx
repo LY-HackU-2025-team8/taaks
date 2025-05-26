58e8a7c559dbc983f2ab7f9fc9b38e833b3b2911
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2025-05-27 01:58:37 +0900

    日付でタスクを絞り込み

--- frontend_react/src/pages/todo/vertical-task-list.tsx
+++ frontend_react/src/pages/todo/vertical-task-list.tsx
@@ -1,11 +1,36 @@
 import { TaskCardLarge } from '@/entities/task/ui/task-card-large';
 import { $api } from '@/shared/api/openapi-fetch';
+import { SERVER_DATETIME_FORMAT } from '@/shared/constant';
+import { format } from 'date-fns';
 
-export const VerticalTaskList = () => {
-  const { data: tasks } = $api.useQuery('get', '/tasks');
+export type VerticalTaskListProps = React.ComponentProps<'div'> & {
+  date?: Date;
+};
+
+export const VerticalTaskList = ({ date, ...props }: VerticalTaskListProps) => {
+  const { data: tasks } = $api.useQuery('get', '/tasks', {
+    params: {
+      query: {
+        ...(date && {
+          dueAt_gt: format(date, SERVER_DATETIME_FORMAT),
+          dueAt_lt: format(
+            new Date(
+              date.getFullYear(),
+              date.getMonth(),
+              date.getDate(),
+              23,
+              59,
+              59
+            ),
+            SERVER_DATETIME_FORMAT
+          ),
+        }),
+      },
+    },
+  });
 
   return (
-    <div className="flex flex-col gap-3.5">
+    <div className="flex flex-col gap-3.5" {...props}>
       {tasks?.content?.map((task) => (
         <TaskCardLarge key={task.id} task={task} />
       ))}
