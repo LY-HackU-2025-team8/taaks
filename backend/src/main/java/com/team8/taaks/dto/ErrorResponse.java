7924f0519501997265948410d2f8642f12d21de3
amura870 <muramatsu-atsuya870@g.ecc.u-tokyo.ac.jp>
2025-05-26 21:00:42 +0900

    fix: add class-level description

--- backend/src/main/java/com/team8/taaks/dto/ErrorResponse.java
+++ backend/src/main/java/com/team8/taaks/dto/ErrorResponse.java
@@ -2,4 +2,5 @@ package com.team8.taaks.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record ErrorResponse(@Schema(required=true, description = "エラー時のレスポンス") String message) { }
+@Schema(description = "エラー時のレスポンス")
+public record ErrorResponse(@Schema(required=true) String message) { }
