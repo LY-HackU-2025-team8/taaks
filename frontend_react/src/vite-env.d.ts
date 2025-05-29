5e388f055940d3f689fcc70dc07d4c84d8c14fe0
naokeyn <naokey-1228@outlook.jp>
2025-05-29 21:18:57 +0900

    vite-env.d.ts にfirebaseのapiキーを追加

--- frontend_react/src/vite-env.d.ts
+++ frontend_react/src/vite-env.d.ts
@@ -9,6 +9,34 @@ interface ImportMetaEnv {
    * Tanstack RouterのDevToolsを表示するか
    */
   readonly VITE_SHOW_TANSTACK_ROUTER_DEVTOOLS: string;
+  /**
+   * FirebaseのAPIキー
+   */
+  readonly VITE_FIREBASE_API_KEY: string;
+  /**
+   * Firebaseの認証ドメイン
+   */
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  /**
+   * FirebaseのプロジェクトID
+   */
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  /**
+   * Firebaseのストレージバケット
+   */
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  /**
+   * Firebaseのメッセージング送信者ID
+   */
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  /**
+   * FirebaseのアプリID
+   */
+  readonly VITE_FIREBASE_APP_ID: string;
+  /**
+   * FirebaseのVAPIDキー
+   */
+  readonly VITE_FIREBASE_VAPID_KEY: string;
 }
 
 interface ImportMeta {
