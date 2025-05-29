21c7c84a14b2a73ff2a8bfdcebbd80dd0a996628
naokeyn <naokey-1228@outlook.jp>
2025-05-30 01:43:27 +0900

    Applied format and lint

--- frontend_react/src/app/routes/_app/create-buddy/index.tsx
+++ frontend_react/src/app/routes/_app/create-buddy/index.tsx
@@ -1,8 +1,9 @@
 import { useCreateBuddyStep } from '@/pages/create-buddy/api/use-create-buddy-step';
-import { createFileRoute } from '@tanstack/react-router';
-import { Button } from '@/shared/ui/components/shadcn/button';
 import { TaaksBuddyLogo } from '@/pages/create-buddy/ui/taaks-buddy-logo';
+import { Button } from '@/shared/ui/components/shadcn/button';
 import { useCallback } from 'react';
+import { createFileRoute } from '@tanstack/react-router';
+
 export const Route = createFileRoute('/_app/create-buddy/')({
   component: RouteComponent,
 });
@@ -16,26 +17,24 @@ function RouteComponent() {
   }, [navigate, nextStep.pathname]);
 
   return (
-    <div className='flex flex-col relative h-screen'>
+    <div className="relative flex h-screen flex-col">
       <video
         src="/assets/videos/buddies.mp4"
         autoPlay
         loop
         muted
-        className='absolute -z-10 top-0 left-0 mask-b-from-80% mask-b-to-100% mask-t-from-80% mask-t-to-100%'
+        className="absolute top-0 left-0 -z-10 mask-t-from-80% mask-t-to-100% mask-b-from-80% mask-b-to-100%"
       />
 
       <div className="mt-auto mb-[env(safe-area-inset-bottom)]">
-        <div
-          className="flex bg-background flex-col text-center items-center justify-center gap-6 px-3.5 py-5"
-        >
-          <TaaksBuddyLogo className="w-84 h-12" />
-          <p className='text-2xl font-bold'>
+        <div className="bg-background flex flex-col items-center justify-center gap-6 px-3.5 py-5 text-center">
+          <TaaksBuddyLogo className="h-12 w-84" />
+          <p className="text-2xl font-bold">
             あなたを支える唯一無二の
             <br />
             バディを作成しよう
           </p>
-          <p className='text-sm text-muted-foreground'>
+          <p className="text-muted-foreground text-sm">
             TaaksBuddyはあなたを支えるAIバディーです。
             <br />
             あなたの好きなヘアスタイル、服装にカスタマイズしましょう。
