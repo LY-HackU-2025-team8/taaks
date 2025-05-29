193bdb69ef4f99c8006e1fb046daa90c93172bc2
Naoya Yamamura <nmy998244353@gmail.com>
2025-05-29 22:06:24 +0900

    Remove unnecessary @Service annotation from SuggestedTaskService interface

--- backend/src/main/java/com/team8/taaks/service/SuggestedTaskService.java
+++ backend/src/main/java/com/team8/taaks/service/SuggestedTaskService.java
@@ -2,9 +2,7 @@ package com.team8.taaks.service;
 
 import com.team8.taaks.dto.TaskResponse;
 import com.team8.taaks.model.TaakUser;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface SuggestedTaskService {
   public TaskResponse getSuggestedTasks(TaakUser user);
 }
