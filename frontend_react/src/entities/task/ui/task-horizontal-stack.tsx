061badb630bc147f6d023ba94ec0fab38ca64208
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2025-05-29 20:13:48 +0900

    タスクがない時の表示を改善

--- frontend_react/src/entities/task/ui/task-horizontal-stack.tsx
+++ frontend_react/src/entities/task/ui/task-horizontal-stack.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/shared/lib/utils';
+import { Text } from '@/shared/ui/components/typography/text';
 import type { TaskResponseModel } from '../api/task-model';
 import { TaskCardSummary } from './task-card-summary';
 
@@ -18,9 +19,17 @@ export const TaskHorizontalStack = ({
       className={cn('flex gap-4 overflow-x-auto py-1', className)}
       {...props}
     >
-      {tasks.map((task) => (
-        <TaskCardSummary task={task} key={task.id} className="flex-shrink-0" />
-      ))}
+      {tasks.length ? (
+        tasks.map((task) => (
+          <TaskCardSummary
+            task={task}
+            key={task.id}
+            className="flex-shrink-0"
+          />
+        ))
+      ) : (
+        <Text variant="muted">この日は登録されたタスクがありません</Text>
+      )}
       {children}
     </div>
   );
