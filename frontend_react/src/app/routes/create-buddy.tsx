292286e9ff6b323e9736672ec9968ace49b5f34d
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2025-05-31 12:08:22 +0900

    buddyに設定済みの値が入るように

--- frontend_react/src/app/routes/create-buddy.tsx
+++ frontend_react/src/app/routes/create-buddy.tsx
@@ -1,4 +1,5 @@
 import { createBuddyFormSchema } from '@/pages/create-buddy/api/create-buddy-form-schema';
+import { $api } from '@/shared/api/openapi-fetch';
 import { cn } from '@/shared/lib/utils';
 import { Form } from '@/shared/ui/components/shadcn/form';
 import { useForm } from 'react-hook-form';
@@ -19,14 +20,16 @@ export const Route = createFileRoute('/create-buddy')({
 });
 
 function RouteComponent() {
+  const { data: buddy } = $api.useQuery('get', '/buddy');
+
   const form = useForm<z.infer<typeof createBuddyFormSchema>>({
     resolver: zodResolver(createBuddyFormSchema),
     defaultValues: {
-      nickname: '',
-      hairStyleId: 1,
-      clothesId: 1,
-      colorId: 1,
-      name: '',
+      nickname: buddy?.nickname || '',
+      hairStyleId: buddy?.hairStyleId || 1,
+      clothesId: buddy?.clothesId || 1,
+      colorId: buddy?.colorId || 1,
+      name: buddy?.name || '',
     },
     mode: 'onBlur',
     reValidateMode: 'onChange',
